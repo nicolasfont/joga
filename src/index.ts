915d6af0f34f11cdeddcb81e4f7ce0803bcf6689
Nicolás Font <nicolasfont@gmail.com>
2022-05-12 17:13:33 -0300

    Explicitely re-export emotion functions
    Vite doesn't properly re-export without this intermediate step.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export * from "@emotion/css";
+import { css, cx, injectGlobal, keyframes } from "@emotion/css";
+
 export * from "./Box";
 export * from "./Button";
+export { css, cx, injectGlobal, keyframes };
