44c7beaa4a31eb16dc1da26656b1ecb0ca797fef
Nicolás Font <nicolasfont@gmail.com>
2022-05-13 18:45:59 -0300

    Use Text in Box story

--- src/Box/index.stories.tsx
+++ src/Box/index.stories.tsx
@@ -1,13 +1,18 @@
 import { ComponentMeta } from "@storybook/react";
 import { Box } from "../Box";
 import { Button } from "../Button";
+import { Text } from "../Text";
 
 export default {
   title: "Box",
   component: Box,
 } as ComponentMeta<typeof Box>;
 
-export const Default = () => <Box>Box</Box>;
+export const Default = () => (
+  <Box>
+    <Text>Box</Text>
+  </Box>
+);
 
 export const ButtonStack = () => (
   <Box direction="column" gap={8}>
