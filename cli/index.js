4df09e0dc4bbc02e54236ee55563e67a6e28cbb8
Nicolás Font <nicolasfont@gmail.com>
2022-05-14 17:21:16 -0300

    Add CLI

--- cli/index.js
+++ cli/index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
+const { build } = require("vite");
+const react = require("@vitejs/plugin-react");
 
-console.log("Hello Joga");
+require("yargs")
+  .scriptName("joga")
+  .usage("$0 <cmd> [args]")
+  .command("build", "Build the current project", async () => {
+    await build({
+      plugins: [react()],
+    });
+  })
+  .demandCommand()
+  .recommendCommands()
+  .help()
+  .alias("-h", "--help")
+  .alias("-v", "--version").argv;
